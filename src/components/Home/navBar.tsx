b7e34ef4af104d0cc5647260e82a1c2e39da655f
SydMatters <nicolas.vega@utp.edu.co>
2024-07-11 17:13:13 -0500

    pink

--- src/components/Home/navBar.tsx
+++ src/components/Home/navBar.tsx
@@ -4,7 +4,7 @@ import MoonIcon from "../../icons/moonIcon"
 
 export default function NavBar() {
   return (
-    <header className="bg-pink-600 flex h-16 w-full items-center justify-between px-4 md:px-8 bg-blue-500 text-white">
+    <header className="bg-pink-600 flex h-16 w-full items-center justify-between px-4 md:px-8 text-white">
       <Link href="#" className="flex items-center space-x-2 w-full justify-between"> 
         <span className="text-lg font-medium">Kanban app</span>
         <div className="flex justify-between" /> 
