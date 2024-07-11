fc5f5010dc7c45adb68580f554e2c6c08f38c4a5
stivenkno <stivencano931@gmail.com>
2024-07-11 11:30:27 -0500

    Add toast

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
+"use client";
+
+import { useForm } from 'react-hook-form';
+
 const Login = () => {
+
+    const {register, handleSubmit} = useForm();
+
     return (
         <div>
             <h1>Login Page</h1>
