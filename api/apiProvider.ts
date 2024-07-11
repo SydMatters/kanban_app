446738332c32235b2cc9582e4ea64c4617d1651d
JsGDmzB27 <juan.cano5@utp.edu.co>
2024-07-11 11:09:43 -0500

    RTK query

---
+++ api/apiProvider.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+interface user{
+    name: string,
+    email: string,
+    password: string
+}
+
+export const kanbanApi = createApi({
+    reducerPath: 'KanbanApi',
+    baseQuery: fetchBaseQuery ({ baseUrl: 'https://api-kanban-zgyh.onrender.com/'}),
+    endpoints: (builder) => ({
+        getUsers: builder.query<user[], void>({
+            query: () => 'api/users',
+        })
+    })
+})
+
+export const { useGetUsersQuery } = kanbanApi;
+
+export default kanbanApi;
