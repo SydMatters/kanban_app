070f8a3154955cb8828f54c1b7f9c068f9715057
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2024-07-11 17:07:14 -0500

    Implement methods in apiProvider.ts for the routes in lines 14-18

--- api/apiProvider.ts
+++ api/apiProvider.ts
@@ -1,12 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { User, Section, Task } from '../src/lib/types'
 
+
 export const kanbanApi = createApi({
     reducerPath: 'KanbanApi',
     baseQuery: fetchBaseQuery ({ baseUrl: 'https://api-kanban-zgyh.onrender.com/'}),
     endpoints: (builder) => ({
         getUsers: builder.query<User[], void>({
             query: () => 'api/users',
+        }),
+        getTasks: builder.query<Task[], (User & Section)>({
+            query: ({user_id, section_id}) => `api/users/${user_id}/sections/${section_id}/tasks`,
+        }),
+
+        getTask: builder.query<Task, (User & Section & Task)>({
+            query: ({user_id, section_id, task_id}) => `api/users/${user_id}/sections/${section_id}/tasks/${task_id}`,
+        }),
+
+        createSection: builder.query<Section[], User>({
+            query: ({user_id}) => `api/users/${user_id}/sections`,
         })
     })
 })
