fd2928108987a654cc14255474b12489e6daee13
SydMatters <nicolas.vega@utp.edu.co>
2024-07-12 14:24:03 -0500

    modificar profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -10,7 +10,7 @@ export default function Profile() {
     return (
       <Dialog>
         <DialogTrigger asChild>
-          <Button>Profile</Button>
+          <Button className="px-2">Profile</Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
